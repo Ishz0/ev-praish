ea84cbd2b6fca12d6b0ae2b4ae174fb987b25939
150289 <150289@C79D9K3.ap.corp.arrow.com>
2022-05-18 10:44:39 +0530

    Created a blank project and linked with GitHub.

---
+++ ev-charger/src/test/java/com/praish/EvChargerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvChargerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
